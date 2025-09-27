0d777bacc50caacb0ba3a5c384c7bfbf8dd97592
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-09-27 05:58:59 +0300

    file for JWT authentication

---
+++ feed/middleware.py
@@ -0,0 +1,30 @@
+
+
+import jwt
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+
+class JWTAuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        token = self.get_token_from_request(request)
+        
+        if token:
+            try:
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+                user = User.objects.get(id=payload['user_id'])
+                request.user = user
+            except (jwt.InvalidTokenError, User.DoesNotExist):
+                request.user = None
+        
+        response = self.get_response(request)
+        return response
+
+    def get_token_from_request(self, request):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if auth_header and auth_header.startswith('Bearer '):
+            return auth_header.split(' ')[1]
+        return None
