f19d34fbae373dd01776a1c18d94712d605fb60e
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-09-27 06:10:42 +0300

    defines the views for the application

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return JsonResponse({
+        'message': 'Social Media Feed Backend API',
+        'graphql_endpoint': '/graphql/',
+        'status': 'running'
+    })
+
+def health_check(request):
+    return JsonResponse({
+        'status': 'healthy',
+        'service': 'social_media_feed',
+    })
