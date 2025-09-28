68e66aad7c535f4a4cc2c1c5b81825fc013dad71
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-09-28 22:07:36 +0300

    feat: create models for posts, comments, and interactions

--- feed/models.py
+++ feed/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
+ 
 
 class UserProfile(models.Model):
     """Extended user profile with additional fields"""
