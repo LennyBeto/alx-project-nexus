8e03f0decc6ab1ba92fcbd68eee4e5fd07a2ddfd
Lenny Beto <lennybeto95@gmail.com>
2026-01-28 13:53:02 +0300

    feat: implement like, comment, and share functionalities

---
+++ social-media-feed/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import client from "./graphql/client";
+import { Feed } from "./components/Feed/Feed";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="min-h-screen bg-dark-50">
+          <Routes>
+            <Route path="/" element={<Feed />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
